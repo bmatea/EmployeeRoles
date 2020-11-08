37f947b7bf5a3519f7e1009a16eb9677feb2f0e4
bmatea <mateaboban05@gmail.com>
2020-11-08 10:31:53 +0100

    Added UserRole repo interface

---
+++ Models/Repository/IEmployeeRoleRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zadatak.Models.Repository
+{
+    public interface IEmployeeRoleRepository : IDataRepository<EmployeeRole>
+    {
+        IEnumerable<int> getRolesForEmployee(int employeeId);
+
+        int Add(int employeeId, int roleId);
+        int Delete(int employeeId, int roleId);
+    }
+}
